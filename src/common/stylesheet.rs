8d3d67939f6ea4227033244e9bafbad619cc1020
Federico Razzoli <info@federico-razzoli.com>
2020-06-19 00:12:13 +0100

    Document stylesheet

--- src/common/stylesheet.rs
+++ src/common/stylesheet.rs
@@ -1,3 +1,4 @@
+/// Transformations that can be applied to texts.
 #[derive(Debug, Clone)]
 pub enum StyleTransformation {
     Blink,
@@ -6,6 +7,7 @@ pub enum StyleTransformation {
     Underlined,
 }
 
+/// Colors that can be used for texts and/or their backgrounds.
 #[derive(Debug, Clone)]
 pub enum StyleColor {
     Black,
@@ -15,26 +17,60 @@ pub enum StyleColor {
     Blue,
 }
 
+/// All properties that form a style.
 #[derive(Debug, Clone)]
 pub struct StyleProperties {
+    /// A vector of all the transformations to apply to texts.
+    /// Can be empty. Remember that it's a vector, not an array.
+    ///
+    /// # Examples
+    /// [].to_vec()
+    /// [StyleTransformation.Bold, StyleTransformation.Underlined].to_vec()
     pub transformation: Vec<StyleTransformation>,
+
+    /// The color to apply to the text. The type is Option<StyleColor>.
+    /// None means default.
     pub color: Option<StyleColor>,
+
+    /// The color to apply to the text background. The type is Option<StyleColor>.
+    /// None means default.
     pub background: Option<StyleColor>,
 }
 
 
+/// stylesheet module creates and handles a stylesheet.
+/// A stylesheet is a library of named styles that can be applied to texts.
 pub mod stylesheet {
     use std::collections::HashMap;
 
     extern crate console;
     use self::console::Style;
 
+    /// Return a new stylesheet. It only contains the "default" style,
+    /// that can be used explicitally and is used implicitally when
+    /// we try to use a non-existing style.
     pub fn new<'a>() -> HashMap<&'a str, Style> {
         let mut hash: HashMap<&str, Style> = HashMap::new();
         hash.insert("default", Style::new());
         hash
     }
 
+    /// Add a style to an existing stylesheet.
+    ///
+    /// # Arguments
+    ///
+    /// * `sheet` - A mutable reference to a previously created stylesheet.
+    /// * `style_name` - The name of the style to be added to the stylesheet (&str).
+    /// * `style_definition` - The definition of the style to be added to the stylesheet (StyleProperties).
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// let mut sheet = stylesheet::new();
+    /// sheet::add_style(&mut sheet, "danger", StyleProperties {
+    ///     transformation: [StyleTransformation::Bold, StyleTransformation::Blink].to_vec(), color: Some(StyleColor::Red), background: Some(StyleColor::White)
+    /// });;
+    /// ```
     pub fn add_style(
             sheet: &mut HashMap<&str, Style>,
             style_name: &'static str,
@@ -75,6 +111,22 @@ pub mod stylesheet {
         sheet.insert(style_name, style);
     }
 
+    /// Print a string reference, applying to it a style.
+    ///
+    /// # Arguments
+    ///
+    /// * `message` - The string reference to print.
+    /// * `sheet` - A reference to a previously created stylesheet.
+    /// * `style_name` - The name of the style to use (&str).
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// let mut sheet = stylesheet::new();
+    /// sheet::add_style(&mut sheet, "danger", StyleProperties {
+    ///     transformation: [StyleTransformation::Bold, StyleTransformation::Blink].to_vec(), color: Some(StyleColor::Red), background: Some(StyleColor::White)
+    /// });;
+    /// ```
     pub fn println(
             message: &str,
             sheet: HashMap<&str, Style>,
