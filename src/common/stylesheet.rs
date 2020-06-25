c2be7aae4292d08a6fd790b1cc46b355aafb3a26
Federico Razzoli <info@federico-razzoli.com>
2020-06-25 18:26:59 +0100

    Add Stylesheet.freeze()

--- src/common/stylesheet.rs
+++ src/common/stylesheet.rs
@@ -54,6 +54,7 @@ pub struct StyleProperties {
 /// CSS style plays in an HTML document.
 pub struct Stylesheet {
     styles: HashMap<&'static str, Style>,
+    is_frozen: bool,
 }
 
 impl Stylesheet {
@@ -69,6 +70,7 @@ impl Stylesheet {
 
         Stylesheet {
             styles: hash,
+            is_frozen: false,
         }
     }
 
@@ -104,6 +106,10 @@ impl Stylesheet {
             style_name: &'static str,
             style_definition: StyleProperties
         ) {
+        if self.is_frozen {
+            panic!("FATAL: Trying to add a style to a frozen Stylesheet");
+        }
+
         // style is a handler from console::Style.
         // Based on the contents of style_definition call style functions
         // to create a proper style.
@@ -150,6 +156,25 @@ impl Stylesheet {
         //println!("{:?}", style);
 
         self.styles.insert(style_name, style);
+    }
+
+    /// Freeze the Stylesheet. It will not be possible to modify it again.
+    /// However, trying to freeze it will not cause any complain.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use common::stylesheet::*;
+    /// use common::stylesheet::StyleColor::*;
+    /// use common::stylesheet::StyleTransformation::*;
+    /// let mut sheet = Stylesheet::new();
+    /// sheet.add_style("danger", StyleProperties {
+    ///     transformation: [Bold, Blink,].to_vec(), color: Some(Red), background: Some(White)
+    /// });
+    /// sheet.freeze();
+    /// ```
+    pub fn freeze(&mut self) {
+        self.is_frozen = true;
     }
 
     /// Print a line (string reference), applying to it a single style.
@@ -266,6 +291,36 @@ mod tests {
             }
         );
         assert!(sheet.contains(style_name));
+    }
+
+    #[test]
+    fn freeze() {
+        let mut sheet = Stylesheet::new();
+        // should be able to freeze (become immutable)
+        sheet.freeze();
+        // did not panick
+        assert!(true);
+    }
+
+    #[test]
+    fn freeze_twice() {
+        let mut sheet = Stylesheet::new();
+        // should be able to freeze (become immutable)
+        sheet.freeze();
+        sheet.freeze();
+        // did not panick
+        assert!(true);
+    }
+
+    #[test]
+    #[should_panic(expected = "FATAL:")]
+    fn freeze_and_try_to_modify() {
+        let mut sheet = Stylesheet::new();
+        sheet.freeze();
+        sheet.add_style(
+            "test",
+            StyleProperties { transformation: [].to_vec(), color: None, background: None, }
+        );
     }
 
     #[test]
